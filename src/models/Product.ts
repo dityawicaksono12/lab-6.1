012ae696e5b53daf8197db6955072bb8f868637f
Aditya Wicaksono <dityawicaksono12@gmail.com>
2025-07-13 20:15:47 -0400

    Add Product class

---
+++ src/models/Product.ts
@@ -0,0 +1,17 @@
+class Product {
+    sku: string;
+    name: string;
+    price: number;
+
+constructor(sku: string, name: string, price: number) {
+    this.sku = sku
+    this.name = name
+    this.price = price
+}
+
+displayDetails(): string {
+    return `${this.sku}, ${this.name}, $${this.price}`
+}
+getPriceWithTax(): number {
+    return this.price
+}}
